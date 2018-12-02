575fe68b1c2496d2f169b4fd4594af192cbbd9e8
Nima Hejazi <nh@nimahejazi.org>
2018-12-02 15:44:24 -0800

    add indexing test

--- tests/testthat/test-treatment_indexing.R
+++ tests/testthat/test-treatment_indexing.R
@@ -56,3 +56,33 @@ head(data)
 z_names <- colnames(data)[str_detect(colnames(data), "Z")]
 w_names <- colnames(data)[str_detect(colnames(data), "W")]
 
+
+# get indices of treatment and control units
+idx_treat <- which(data$A == 1)
+idx_cntrl <- which(data$A == 0)
+
+
+# just try indexing by the treatment mechanism
+g_out <- fit_g_mech(
+  data = data,
+  delta = delta,
+  lrnr_stack = Lrnr_hal9001$new(fit_type = "glmnet", family = "binomial"),
+  w_names = w_names
+)
+
+g_shifted_treat <- g_out$g_est$g_pred_A1
+g_shifted_cntrl <- g_out$g_est$g_pred_A0
+
+# construct shifted vector based on indexing
+g_shifted_idx <- rep(NA, length(g_shifted_treat))
+g_shifted_idx[idx_treat] <- g_shifted_treat[idx_treat]
+g_shifted_idx[idx_cntrl] <- g_shifted_cntrl[idx_cntrl]
+
+# construct shifted vector based on treatment vector  multiplication
+g_shifted_mult <- (g_shifted_treat * data$A) + g_shifted_cntrl * (1 - data$A)
+
+# test that indexing approaches produce identical results
+test_that("Different indexing approaches produce identical results", {
+  expect_identical(g_shifted_idx, g_shifted_mult)
+})
+
