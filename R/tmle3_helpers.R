2b39a9112171ccf9cbd96f624e361f75f56bf75f
Nima Hejazi <nh@nimahejazi.org>
2019-04-03 12:50:59 -0700

    add NPSEM and likelihood factors + mock tests

---
+++ R/tmle3_helpers.R
@@ -0,0 +1,61 @@
+#' Stochastic Mediation NPSEM
+#'
+#' @param node_list ...
+#' @param variable_types ...
+#'
+#' @export
+#
+stochastic_mediation_npsem <- function(node_list, variable_types = NULL) {
+  # make tmle_task
+  npsem <- list(
+    define_node("W", node_list$W, variable_type = variable_types$W),
+    define_node("A", node_list$A, c("W"), variable_type = variable_types$A),
+    define_node("Z", node_list$Z, c("A", "W"), variable_type = variable_types$Z),
+    define_node("eA", node_list$A, c("Z", "W"), variable_type = variable_types$A),
+    define_node("Y", node_list$Y, c("Z", "A", "W"), variable_type = variable_types$Y, scale = TRUE)
+  )
+  return(npsem)
+}
+
+
+
+#' Stochastic Mediation Likelihood Factors
+#'
+#' @param tmle_task ...
+#' @param learner_list ...
+#'
+#' @export
+#
+stochastic_mediation_likelihood <- function(tmle_task, learner_list) {
+  # covariates
+  W_factor <- define_lf(LF_emp, "W")
+
+  # treatment (bound likelihood away from 0 (and 1 if binary))
+  A_type <- tmle_task$npsem[["A"]]$variable_type
+  if (A_type$type == "continuous") {
+    A_bound <- c(1 / tmle_task$nrow, Inf)
+  } else if (A_type$type %in% c("binomial","categorical")) {
+    A_bound <- 0.025
+  } else {
+    A_bound <- NULL
+  }
+
+  # treatment
+  A_factor <- define_lf(LF_fit, "A", learner = learner_list[["A"]],
+                        bound = A_bound)
+
+  # mediator re-parametrization
+  eA_factor <- define_lf(LF_fit, "eA", learner = learner_list[["eA"]],
+                         bound = "mean")
+
+  # outcome
+  Y_factor <- define_lf(LF_fit, "Y", learner = learner_list[["Y"]],
+                        type = "mean")
+
+  # construct and train likelihood
+  factor_list <- list(W_factor, A_factor, eA_factor, Y_factor)
+
+  likelihood_def <- Likelihood$new(factor_list)
+  likelihood <- likelihood_def$train(tmle_task)
+  return(likelihood)
+}
---
+++ tests/testthat/test-tmle_binary.R
@@ -0,0 +1,104 @@
+context("TML estimator for incremental propensity score interventions")
+
+library(data.table)
+library(stringr)
+library(future)
+library(hal9001)
+library(sl3)
+library(tmle3)
+set.seed(7128816)
+delta <- 0.5
+
+################################################################################
+# setup learners for the nuisance parameters
+################################################################################
+
+# instantiate some learners
+mean_lrnr <- Lrnr_mean$new()
+fglm_contin_lrnr <- Lrnr_glm_fast$new()
+fglm_binary_lrnr <- Lrnr_glm_fast$new(family = binomial())
+hal_contin_lrnr <- Lrnr_hal9001$new(
+  fit_type = "glmnet", n_folds = 5
+)
+hal_binary_lrnr <- Lrnr_hal9001$new(
+  fit_type = "glmnet", n_folds = 5,
+  family = "binomial"
+)
+
+################################################################################
+# setup data and simulate to test with estimators
+################################################################################
+make_simulated_data <- function(n_obs = 1000, # no. observations
+                                n_w = 3, # no. baseline covariates
+                                delta = 0.5) { # shift parameter value
+
+  # baseline covariate -- simple, binary
+  W_1 <- rbinom(n_obs, 1, prob = 0.50)
+  W_2 <- rbinom(n_obs, 1, prob = 0.65)
+  W_3 <- rbinom(n_obs, 1, prob = 0.35)
+  W <- cbind(W_1, W_2, W_3)
+
+  # create treatment based on baseline W
+  A <- as.numeric(rbinom(n_obs, 1, prob = (rowSums(W) / 4 + 0.1)))
+
+  # mediators to affect the outcome
+  ## 1st mediator (binary)
+  z1_prob <- 1 - plogis((A^2 + W[, 1]) / (A + W[, 1]^3 + 0.5))
+  z1_prob[z1_prob < 0.01] <- 0.01
+  z1_prob[z1_prob > 0.99] <- 0.99
+  Z_1 <- rbinom(n_obs, 1, prob = z1_prob)
+  ## 2nd mediator (binary)
+  z2_prob <- plogis((A - 1)^3 + W[, 2] / (W[, 3] + 3))
+  z2_prob[z2_prob < 0.01] <- 0.01
+  z2_prob[z2_prob > 0.99] <- 0.99
+  Z_2 <- rbinom(n_obs, 1, prob = z2_prob)
+  ## 3rd mediator (binary)
+  z3_prob <- plogis((A - 1)^2 + 2 * W[, 1]^3 - 1 / (2 * W[, 1] + 0.5))
+  z3_prob[z3_prob < 0.01] <- 0.01
+  z3_prob[z3_prob > 0.99] <- 0.99
+  Z_3 <- rbinom(n_obs, 1, prob = z3_prob)
+  ## build matrix of mediators
+  Z <- cbind(Z_1, Z_2, Z_3)
+
+  # create outcome as a linear function of A, W + white noise
+  Y <- Z_1 + Z_2 - Z_3 + A - 0.1 * rowSums(W)^2 +
+    rnorm(n_obs, mean = 0, sd = 0.5)
+
+  # full data structure
+  data <- as.data.table(cbind(Y, Z, A, W))
+  setnames(data, c(
+    "Y", paste("Z", 1:3, sep = "_"), "A",
+    paste("W", seq_len(dim(W)[2]), sep = "_")
+  ))
+  return(data)
+}
+
+# get data and column names for sl3 tasks (for convenience)
+data <- make_simulated_data()
+z_names <- colnames(data)[str_detect(colnames(data), "Z")]
+w_names <- colnames(data)[str_detect(colnames(data), "W")]
+
+# set up TMLE components: NPSEM, likelihood
+npsem <- list(
+  define_node("W", c(
+    "W_1", "W_2", "W_3"
+  )),
+  define_node("A", c("A"), c("W")),
+  define_node("Z", c("Z_1", "Z_2", "Z_3"), c("A", "W")),
+  define_node("eA", c("A"), c("Z", "W")),
+  define_node("Y", c("Y"), c("Z", "A", "W"))
+)
+
+factor_list <- list(
+  define_lf(LF_emp, "W"),
+  define_lf(LF_fit, "A", hal_binary_lrnr),
+  define_lf(LF_fit, "eA", hal_binary_lrnr),
+  define_lf(LF_fit, "Y", hal_contin_lrnr, type = "mean")
+)
+
+# create TMLE task
+tmle_task <- tmle3_Task$new(data, npsem = npsem)
+likelihood_def <- Likelihood$new(factor_list)
+likelihood <- likelihood_def$train(tmle_task)
+likelihood$get_likelihoods(tmle_task)
+
